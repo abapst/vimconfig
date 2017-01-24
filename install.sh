b3b5d2835209db3af77475a006872151502a0499
Aleksander <alekbapst@gmail.com>
2017-01-23 21:43:04 -0700

    add libgtk2.0 for fullscreen support

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 # Attempt to install gvim
-echo "Attempting to install gvim..."
-sudo apt-get install vim-gtk > /dev/null
+sudo apt-get install vim-gtk
 
+# Install libgtk2.0 for fullscreen support in Ubuntu 16.04
+sudo apt-get install libgtk2.0
+
+# Add gvim alias to bashrc
 if grep -Fxq "alias vim=/usr/bin/gvim" $HOME/.bashrc
 then
   :
@@ -9,8 +12,10 @@ else
   echo "alias vim=/usr/bin/gvim" >> $HOME/.bashrc
 fi
 
+# Redirect vimrc to .vim folder
 echo "runtime vimrc" > $HOME/.vimrc
 
+# Copy modified gtkrc script to home
 cp -a dotfiles/. $HOME
 
 echo "Done!"
